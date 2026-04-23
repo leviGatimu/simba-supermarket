f7693687e79601bcaef188d256d70c72243702ad
Levi Gatimu <getmorelev@gmail.com>
2026-04-23 08:48:16 +0200

    feat: add show special product toggle

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,26 @@ import Hero from './components/Hero'
 import Footer from './components/Footer'
 import ProductCard from './components/ProductCard'
 import { PRODUCTS } from './data/products'
-
+import { useState } from "react";
 function App() {
-  // Select the first product to render
   const p = PRODUCTS[0];
-
+  const [showSpecial, setShowSpecial] = useState(false);
   return (
     <>
       <Header />
       <main id="center">
         <Hero />
-        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-          <ProductCard product={p} />
+      {!showSpecial ? (
+        <div style={{padding:'30px'}}>
+          <button onClick={()=>setShowSpecial(true)} style={{padding:'10px', border: 'none' , borderRadius: '10px', backgroundColor: 'blue'}}> Show Today's Special</button>
         </div>
+      ):(
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+        <ProductCard product={p} />
+        </div>
+        )
+      }
+        
       </main>
       <Footer />
     </>
